8cc7d8ce01b5d8d34bc2422d65cbc004ebb55c09
David Roman <romanleon2010@hotmail.com>
2021-06-11 18:09:31 -0500

    fin seccion 4 -Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,38 +1,33 @@
+//moduloes
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+
+import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
+import { AuthRoutingModule } from './auth/auth.routing';
+
 
 
 //luego de iniciar sesion, me va a redirigir a PagesComponent donde esta el header, footer, breadcrumbs etc
 // y dentro de este tenemos el <router-outlet> que es donde se mostraran las paginas hijas, en este caso dashboard, progress, grafica1
 //las paginas publicas seran login, register
 const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    ]
-  },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
 
+  //path: '/dashboard' esta en PagesRouting
+  //path: '/progress' esta en PagesRouting
+  //path: '/grafica1' esta en PagesRouting
+  //path: '/auth' esta en AuthRouting
   { path: '**', component: NotpagefoundComponent},
 ]
 
 
 @NgModule({
   declarations: [],
-  imports: [ RouterModule.forRoot( routes ) ],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
+  ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
